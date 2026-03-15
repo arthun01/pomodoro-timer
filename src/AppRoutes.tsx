56392aa59a5658931f3d4050ec6dfa5ce8d8248d
Arthur Ramos Vieira <arthurramos7890@gmail.com>
2026-03-15 14:44:54 -0300

    add a simple theme for our app

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,17 +2,47 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import { Settings } from './pages/Settings';
 import { Home } from './pages/Home';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, NavigationProp, DefaultTheme } from '@react-navigation/native';
+import { Theme } from './shared/themes/Theme';
 
-const Stack = createStackNavigator();
+type TScreenDefinitions = {
+  Home: undefined;
+  Settings: undefined;
+}
+
+const Stack = createStackNavigator<TScreenDefinitions>();
 
 export default function AppRoutes() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+    <NavigationContainer
+      theme={{
+        ...DefaultTheme,
+        fonts: {
+          ...DefaultTheme.fonts,
+          bold: {
+            fontWeight: '700',
+            fontFamily: Theme.fonts.interBold,
+          },
+          regular: {
+            fontWeight: '500',
+            fontFamily: Theme.fonts.interRegular,
+          },
+        },
+        colors: {
+          ...DefaultTheme.colors,
+          background: Theme.colors.background,
+          primary: Theme.colors.primary,
+          text: Theme.colors.text,
+          card: Theme.colors.divider,
+        }
+      }}
+    >
+      <Stack.Navigator initialRouteName='Home' screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Settings" component={Settings} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export type TNavigationScreenProps = NavigationProp<TScreenDefinitions>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,20 @@
 import { useNavigation } from '@react-navigation/native';
 import { Text, View, TouchableOpacity } from 'react-native';
+import { TNavigationScreenProps } from '../AppRoutes';
+import { Theme } from '../shared/themes/Theme';
 
 export const Home = () => {
-    const navigation = useNavigation<any>();
+    const navigation = useNavigation<TNavigationScreenProps>();
 
     return (
         <View>
-            <Text style={{ fontFamily: 'InterBold', fontSize: 32 }}>
+            <Text style={{ fontFamily: 'InterBold', fontSize: 32, color: Theme.colors.text }}>
                 ZenFocus
             </Text>
 
 
             <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
-                <Text style={{ fontFamily: 'InterRegular', fontSize: 18 }}>
+                <Text style={{ color: Theme.colors.text, fontFamily: Theme.fonts.interRegular }}>
                     Settings
                 </Text>
             </TouchableOpacity>
