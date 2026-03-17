9b13b4f13e4617f8cec2a003dffc1798e28f5db4
Arthur Ramos Vieira <arthurramos7890@gmail.com>
2026-03-16 22:14:00 -0300

    finish static home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { Text, View, TouchableOpacity } from 'react-native';
 import { TNavigationScreenProps } from '../AppRoutes';
 import { Theme } from '../shared/themes/Theme';
 import { StyleSheet } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 import CircularProgress from 'react-native-circular-progress';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
@@ -11,21 +12,72 @@ export const Home = () => {
     const navigation = useNavigation<TNavigationScreenProps>();
 
     return (
-        <View>
-            <View style={styles.progressContainer}>
-                <AnimatedCircularProgress
-                size={160}
-                width={8}
-                fill={90}
-                tintColor={Theme.colors.divider}
-                rotation={0}
-                backgroundColor={Theme.colors.primary}
-                children={() => (
-                    <Text style={styles.progressText}>
-                        12:45
+        <View style={styles.container}>
+            {/* Botão Settings */}
+            <View style={styles.mainContainer}>
+                <TouchableOpacity style={styles.settingsButton} onPress={() => navigation.navigate('Settings')}>
+                    <Ionicons name="settings" size={28} color={Theme.colors.divider} />
+                </TouchableOpacity>
+            </View>
+
+            {/* Título, States e Progress */}
+            <View style={styles.titleGroup}>
+                {/* TÍTULO */}
+                <View style={styles.titleContainer}>
+                    <Text style={styles.titleText}>
+                        ZenFocus
                     </Text>
-                )}
-                />
+                </View>
+
+                {/* STATES */}
+                <View>
+                    <View style={styles.stateContainer}>
+                        <Text style={styles.stateText}>
+                            Vamos nos concentrar?
+                        </Text>
+                    </View>
+
+                    <View style={styles.stateContainer}>
+                        <Text style={styles.stateText}>
+                            Hora de se concentrar!
+                        </Text>
+                    </View>
+
+                    <View style={styles.stateContainer}>
+                        <Text style={styles.stateText}>
+                            Pausa curta
+                        </Text>
+                    </View>
+
+                    <View style={styles.stateContainer}>
+                        <Text style={styles.stateText}>
+                            Pausa longa
+                        </Text>
+                    </View>
+
+                    <View style={styles.stateContainer}>
+                        <Text style={styles.stateText}>
+                            Cronômetro em pausa
+                        </Text>
+                    </View>
+                </View>
+
+                {/* PROGRESS */}
+                <View style={styles.progressContainer}>
+                    <AnimatedCircularProgress
+                    size={160}
+                    width={8}
+                    fill={43}
+                    tintColor={Theme.colors.divider}
+                    rotation={0}
+                    backgroundColor={Theme.colors.primary}
+                    children={() => (
+                        <Text style={styles.progressText}>
+                            12:45
+                        </Text>
+                    )}
+                    />
+                </View>
             </View>
 
             {/* Botão Iniciar */}
@@ -37,7 +89,7 @@ export const Home = () => {
                 </TouchableOpacity>
             </View>
 
-            {/* Botôes Pausar e Parar */}
+            Botôes Pausar e Parar
             <View style={styles.buttonContainer}>
                 <TouchableOpacity style={styles.primaryButton}>
                     <Text style={styles.primaryButtonText}>
@@ -66,11 +118,44 @@ export const Home = () => {
                     </Text>
                 </TouchableOpacity>
             </View>
+
+            {/* Pomodoros */}
+            <View style={styles.pomodorosContainer}>
+                <Text style={styles.pomodorosText}>
+                    Pomodoros: 
+                </Text>
+
+                <View style={styles.pomodorosIndicatorComplete} />
+                <View style={styles.pomodorosIndicatorComplete} />
+                <View style={styles.pomodorosIndicatorComplete} />
+                <View style={styles.pomodorosIndicatorIncomplete} />
+                <View style={styles.pomodorosIndicatorIncomplete} />
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 36,
+        backgroundColor: Theme.colors.background,
+    },
+    mainContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        width: '100%',
+    },
+    titleGroup: {
+        gap: 24,
+    },
+    settingsButton: {
+        alignSelf: 'flex-end',
+    },
     primaryButton:{
         backgroundColor: Theme.colors.primary,
         paddingHorizontal: 24,
@@ -106,5 +191,44 @@ const styles = StyleSheet.create({
         color: Theme.colors.text,
         fontFamily: Theme.fonts.interBold,
         fontSize: Theme.fontSizes.title,
-    }
+    },
+    titleContainer: {
+        alignItems: 'center',
+    },
+    titleText: {
+        color: Theme.colors.text,
+        fontFamily: Theme.fonts.interBold,
+        fontSize: Theme.fontSizes.title,
+    },
+    stateContainer: {
+        alignItems: 'center',
+    },
+    stateText: {
+        color: Theme.colors.text,
+        fontFamily: Theme.fonts.interRegular,
+        fontSize: Theme.fontSizes.body,
+    },
+    pomodorosContainer: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 8,
+    },
+    pomodorosText: {
+        color: Theme.colors.text,
+        fontFamily: Theme.fonts.interRegular,
+        fontSize: Theme.fontSizes.body,
+    },
+    pomodorosIndicatorComplete: {
+        width: 20,
+        height: 20,
+        borderRadius: '100%',
+        backgroundColor: Theme.colors.primary,
+    },
+    pomodorosIndicatorIncomplete: {
+        width: 20,
+        height: 20,
+        borderRadius: '100%',
+        backgroundColor: Theme.colors.divider,
+    },
 })
